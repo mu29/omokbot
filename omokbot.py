79d50df470cd4c1380c6b72d00432babbd3d649e
InJung Chung <mu29gl@gmail.com>
2015-09-05 19:15:19 +0900

    dispatch messages

--- omokbot.py
+++ omokbot.py
@@ -1,16 +1,39 @@
 # coding: utf-8
 
+import time
 from slackclient import SlackClient
+from message import *
 from settings import *
 
 class OmokBot(object):
 	def __init__(self):
 		self.client = SlackClient(TOKEN)
 
+	def dispatch_message(self, events):
+		messages = []
+		for e in events:
+			channel = e.get('channel', '')
+			text = e.get('text', '')
+			command = text.split(' ')[0]
+			content = text.replace(command + ' ', '')
+
+			msg = Message()
+			msg.set(text, command, content)
+			messages.append(msg)
+
+			print(channel + "," + command + "," + content);
+
+		return messages
+
 	def run(self):
 		self.client.rtm_connect()
-		self.client.rtm_send_message('random', u'슬랙 봇 테스트')
-		print("!")
+		#self.client.rtm_send_message('random', u'슬랙 봇 테스트')
+		while True:
+			events = self.client.rtm_read()
+			if events:
+				messages = self.dispatch_message(events)
+			time.sleep(1)
+
 
 
 if '__main__' == __name__:
