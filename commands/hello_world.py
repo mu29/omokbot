0e4295d3072c879baf812658281e09386aa7cdf3
InJung Chung <mu29gl@gmail.com>
2015-09-05 20:25:49 +0900

    omok test

--- commands/hello_world.py
+++ commands/hello_world.py
@@ -2,6 +2,18 @@
 
 from omokbot import OmokBot
 from message import Message
+import random
 
 def run(bot, message):
-    bot.client.rtm_send_message(message.channel, u'그래 안녕')
+    board = "";
+    for y in range(0, 15):
+        for x in range(0, 15):
+            i = random.randint(0, 2)
+            if i == 0:
+                board += "⚪️"
+            elif i == 1:
+                board += "⚫️"
+            else:
+                board += "⬜️"
+        board += "\n"
+    bot.client.rtm_send_message(message.channel, board)
