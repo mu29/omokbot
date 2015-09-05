1d255abae4c70814d2edff954111ac3a25709537
InJung Chung <mu29gl@gmail.com>
2015-09-05 21:21:50 +0900

    fix start_game command to avoid multi play

--- commands/start_game.py
+++ commands/start_game.py
@@ -5,6 +5,9 @@ from message import Message
 from game import Game
 
 def run(bot, msg):
-    bot.game[msg.user] = Game()
-    bot.client.rtm_send_message(msg.channel, u"게임을 시작하지")
-    bot.client.rtm_send_message(msg.channel, bot.game[msg.user].get_board())
+    if msg.user in bot.game:
+        bot.client.rtm_send_message(msg.channel, u"하던 게임부터 마무리해주렴")
+    else:
+        bot.game[msg.user] = Game()
+        bot.client.rtm_send_message(msg.channel, u"게임을 시작하지")
+        bot.client.rtm_send_message(msg.channel, bot.game[msg.user].get_board())
