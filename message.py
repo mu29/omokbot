4a92ecd4e4de850312481a4920cbdef36eab88bb
InJung Chung <mu29gl@gmail.com>
2015-09-05 20:38:38 +0900

    fix message class

--- message.py
+++ message.py
@@ -2,11 +2,13 @@
 
 class Message(object):
     def __init__(self):
+        self.user = None
         self.channel = None
         self.command = None
         self.content = None
 
-    def set(self, channel, command, content):
+    def set(self, user, channel, command, content):
+        self.user = user
         self.channel = channel
         self.command = command
         self.content = content
