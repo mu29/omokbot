ca9ee2dd027a0b9b4b1f204ead3fb081d2fbe20e
InJung Chung <mu29gl@gmail.com>
2015-09-06 23:03:29 +0900

    apply judge function

--- commands/put_block.py
+++ commands/put_block.py
@@ -11,6 +11,10 @@ def run(bot, msg):
         bot.client.rtm_send_message(msg.channel, u"잘 좀 입력해 봐")
         return
 
+    if not msg.user in bot.game:
+        bot.client.rtm_send_message(msg.channel, u"게임 중인거 맞아?")
+        return
+
     x = ord(msg.content[0]) - 65
     y = int(msg.content[1:]) - 1
     pos = y * 15 + x
@@ -26,5 +30,13 @@ def run(bot, msg):
     game.board = game.board[:pos] + BLACK_BLOCK + game.board[pos + 1:]
     bot.client.rtm_send_message(msg.channel, bot.game[msg.user].get_board())
 
+    if game.judge == BLACK_BLOCK:
+        bot.client.rtm_send_message(msg.channel, u"니가 이겼어 ㄷㄷ")
+        del(bot.game[msg.user])
+
     game.ai_turn()
     bot.client.rtm_send_message(msg.channel, bot.game[msg.user].get_board())
+
+    if game.judge == WHITE_BLOCK:
+        bot.client.rtm_send_message(msg.channel, u"내가 이겼네?")
+        del(bot.game[msg.user])
